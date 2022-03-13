be1243a5c8f3a5bb829c82e3ca53e603a6142857
Pablo Leal <pabloleal7@hotmail.com>
2022-03-13 14:18:27 -0300

    Player GET All Controller

--- src/main/java/com/example/apijokenpo/controller/PlayerController.java
+++ src/main/java/com/example/apijokenpo/controller/PlayerController.java
@@ -7,12 +7,10 @@ import com.example.apijokenpo.service.PlayerService;
 import com.example.apijokenpo.util.ConverterUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/player")
@@ -27,6 +25,12 @@ public class PlayerController {
         PlayerResponseDTO response = playerService.create(createPlayerDTO);
         return ResponseEntity.created(URI.create("http://localhost:8080/player/" + response.getId())).build();
 
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlayerResponseDTO>> getAll(){
+        List<PlayerResponseDTO> responseList = playerService.getAll();
+        return ResponseEntity.ok(responseList);
     }
 
 
