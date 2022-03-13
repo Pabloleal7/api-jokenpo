05e9089d4ed16c4577f2bf3778bfbe1e7a397a82
Pablo Leal <pabloleal7@hotmail.com>
2022-03-13 14:10:21 -0300

    player URI.create fix

--- src/main/java/com/example/apijokenpo/controller/PlayerController.java
+++ src/main/java/com/example/apijokenpo/controller/PlayerController.java
@@ -2,19 +2,30 @@ package com.example.apijokenpo.controller;
 
 
 import com.example.apijokenpo.dto.request.CreatePlayerDTO;
+import com.example.apijokenpo.dto.response.PlayerResponseDTO;
+import com.example.apijokenpo.service.PlayerService;
+import com.example.apijokenpo.util.ConverterUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("/player")
 public class PlayerController {
 
+    @Autowired
+    PlayerService playerService;
+
 
     @PostMapping
-    public void createPlayer(@RequestBody CreatePlayerDTO createPlayerDTO){
-
+    public ResponseEntity<Void> createPlayer(@RequestBody CreatePlayerDTO createPlayerDTO) {
+        PlayerResponseDTO response = playerService.create(createPlayerDTO);
+        return ResponseEntity.created(URI.create("http://localhost:8080/player/" + response.getId())).build();
 
     }
 
