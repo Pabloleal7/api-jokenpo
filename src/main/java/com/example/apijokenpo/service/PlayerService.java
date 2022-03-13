a33491a5e6d09fbd52e3c9d2ec6cb61da9eec251
Pablo Leal <pabloleal7@hotmail.com>
2022-03-13 14:15:17 -0300

    Player Service getAll and getAllStream

--- src/main/java/com/example/apijokenpo/service/PlayerService.java
+++ src/main/java/com/example/apijokenpo/service/PlayerService.java
@@ -8,19 +8,34 @@ import com.example.apijokenpo.util.ConverterUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Service
 public class PlayerService {
 
     @Autowired
     PlayerRepository playerRepository;
 
-    public PlayerResponseDTO create(CreatePlayerDTO createPlayerDTO){
+    public PlayerResponseDTO create(CreatePlayerDTO createPlayerDTO) {
         PlayerEntity entity = ConverterUtil.toEntity(createPlayerDTO);
 
-        PlayerEntity savedEntity =  playerRepository.save(entity);
+        PlayerEntity savedEntity = playerRepository.save(entity);
 
         PlayerResponseDTO dto = ConverterUtil.toDTO(savedEntity);
-        return  dto;
+        return dto;
+    }
+
+    public List<PlayerResponseDTO> getAll() {
+
+        return getAllStream().collect(Collectors.toList());
+
+    }
+
+    public Stream<PlayerResponseDTO> getAllStream() {
+        return playerRepository.findAll().stream()
+                .map(ConverterUtil::toDTO);
     }
 
 }
