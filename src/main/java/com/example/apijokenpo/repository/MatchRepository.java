49ec0833bf006247b8e113a617adfe8500a679ef
Pablo Leal <pabloleal7@hotmail.com>
2022-03-14 13:08:10 -0300

    Fix Import

--- src/main/java/com/example/apijokenpo/repository/MatchRepository.java
+++ src/main/java/com/example/apijokenpo/repository/MatchRepository.java
@@ -1,6 +1,7 @@
 package com.example.apijokenpo.repository;
 
 import com.example.apijokenpo.entity.InputEntity;
+import com.example.apijokenpo.entity.MatchEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MatchRepository extends JpaRepository<MatchEntity,Long> {
